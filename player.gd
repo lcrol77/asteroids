6f22892d69903f2ed93662ad56dff1a8329ec695
Logan Croley <logan.croley.18@gmail.com>
2024-09-05 21:28:09 -0400

    added player movement

--- player.gd
+++ player.gd
@@ -2,8 +2,15 @@ extends CharacterBody2D
 
 @onready var crosshair: Sprite2D = %Crosshair
 
+@export var speed = 400
+
 func _ready() -> void:
 	pass
+
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * speed
+	move_and_slide()
 
 func _process(delta: float) -> void:
 	var vec = crosshair.position- position
