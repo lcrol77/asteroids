ad74b6818760b93ee35f0231ac1c14126951f5d7
Logan Croley <logan.croley.18@gmail.com>
2024-09-07 11:54:26 -0400

    added enemy scene

---
+++ enemy.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+
+@onready var player = get_tree().get_first_node_in_group("player")
+@export var speed = 200
+
+func _physics_process(delta):
+	var direction = position.direction_to(player.position)
+	velocity = direction * speed
+	move_and_collide(velocity * delta)
+aaw
