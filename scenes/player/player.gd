b337fe6434aaaad1f05daf28f8c74d9559f12d75
Logan Croley <logan.croley.18@gmail.com>
2024-09-08 11:17:46 -0400

    spawning bullets

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
-@onready var crosshair: Sprite2D = %Crosshair
-
 @export var speed = 400
+@onready var crosshair: Sprite2D = %Crosshair
+@onready var bullet_spawn = $BulletSpawn
+@onready var bullets = $Bullets
 
-func _ready() -> void:
-	pass
+const bullet: PackedScene = preload("res://scenes/bullet/bullet.tscn")
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("left", "right", "up", "down")
@@ -15,3 +15,9 @@ func _physics_process(delta: float) -> void:
 func _process(delta: float) -> void:
 	var vec = crosshair.position- position
 	rotation = vec.angle()
+	
+	# TODO: need to implement some kind of fire rate
+	if Input.is_mouse_button_pressed( 1 ):
+		var instance = bullet.instantiate()
+		instance.global_position = bullet_spawn.global_position
+		bullets.add_child(instance)
