55856a987e3338b47ae833190cf8d3c9c4262901
Logan Croley <logan.croley.18@gmail.com>
2024-09-12 19:48:53 -0400

    asset overhaul

--- player/player.gd
+++ player/player.gd
@@ -1,13 +1,13 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed = 600
 @onready var crosshair: Sprite2D = %Crosshair
 @onready var bullet_spawn = $BulletSpawn
 @onready var bullet_timer: Timer = $BulletTimer
 
 var can_shoot = true
 
-const bullet: PackedScene = preload("res://bullet/bullet.tscn")
+const bullet: PackedScene = preload("res://player/bullet/bullet.tscn")
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("left", "right", "up", "down")
@@ -21,8 +21,10 @@ func _process(_delta: float) -> void:
 	# TODO: need to implement some kind of fire rate
 	if Input.is_mouse_button_pressed( 1 ):
 		if can_shoot:
-			var instance = bullet.instantiate()
+			var instance: Bullet = bullet.instantiate()
 			instance.global_position = bullet_spawn.global_position
+			instance.rotation = rotation
+			instance.target = crosshair.global_position
 			add_child(instance)
 			bullet_timer.start()
 			can_shoot = false
