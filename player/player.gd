7ab0ebaac53461b218225e4e876ecb6d939e308d
Logan Croley <logan.croley.18@gmail.com>
2024-09-12 16:28:52 -0400

    refactor and added bullet fire rate timer

--- player/player.gd
+++ player/player.gd
@@ -3,21 +3,30 @@ extends CharacterBody2D
 @export var speed = 400
 @onready var crosshair: Sprite2D = %Crosshair
 @onready var bullet_spawn = $BulletSpawn
-@onready var bullets = $Bullets
+@onready var bullet_timer: Timer = $BulletTimer
 
-const bullet: PackedScene = preload("res://scenes/bullet/bullet.tscn")
+var can_shoot = true
+
+const bullet: PackedScene = preload("res://bullet/bullet.tscn")
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
 	move_and_slide()
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	var vec = crosshair.position- position
 	rotation = vec.angle()
 	
 	# TODO: need to implement some kind of fire rate
 	if Input.is_mouse_button_pressed( 1 ):
-		var instance = bullet.instantiate()
-		instance.global_position = bullet_spawn.global_position
-		bullets.add_child(instance)
+		if can_shoot:
+			var instance = bullet.instantiate()
+			instance.global_position = bullet_spawn.global_position
+			add_child(instance)
+			bullet_timer.start()
+			can_shoot = false
+
+
+func _on_bullet_timer_timeout() -> void:
+	can_shoot = true
