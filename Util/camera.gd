91f0dbd8c24b612b3d439901cee67e4ecbe90a12
Logan Croley <logan.croley.18@gmail.com>
2024-09-13 14:49:58 -0400

    more camera tweeks, still have a bug where I have ghosting around the crosshair

--- Util/camera.gd
+++ Util/camera.gd
@@ -11,14 +11,8 @@ func lean_camera_towards_mouse_(delta:float) -> void:
 	
 	var direction_to_mouse := (mouse_position - position).normalized()
 	var distance_to_mouse :=  mouse_position.distance_to(position)
-	#var lean := direction_to_mouse * distance_to_mouse * scale_lean
 	var lean := (mouse_position - position) * scale_lean
-	# offset = lean <--- this would work fine but we lerp it to make it smoother
 	offset = lerp(offset, lean, delta * smooth_lean)
-
-	#$debug.text = "direction_to_mouse:%.2f, %.2f" % [direction_to_mouse.x, direction_to_mouse.y]
-	#$debug.text += "\ndistance_to_mouse:%.2f" % distance_to_mouse
-	#$debug.text += "\nlean:%.2f, %.2f" % [lean.x, lean.y]
 
 func match_player_position_() -> void:
 	position = player.position
