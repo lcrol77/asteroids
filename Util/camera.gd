8261062100fd9b5ab84d68630b44f7f60f68b0f0
Logan Croley <logan.croley.18@gmail.com>
2024-09-14 14:48:55 -0400

    added dustins camera changes

--- Util/camera.gd
+++ Util/camera.gd
@@ -6,6 +6,8 @@ extends Camera2D
 const smooth_lean := 10.0
 const scale_lean  := 0.2
 
+var last_physics_pos
+
 func lean_camera_towards_mouse_(delta:float) -> void:
 	var mouse_position := crosshair.global_position
 	
@@ -16,7 +18,13 @@ func lean_camera_towards_mouse_(delta:float) -> void:
 
 func match_player_position_() -> void:
 	position = player.position
+	
+func _process(delta) -> void:
+	if Engine.get_frames_per_second() > 60: # This is whatever your physics fps is
+		var lerp_weight = Engine.get_physics_interpolation_fraction()
+		position = last_physics_pos.lerp(player.position, lerp_weight)
 
 func _physics_process(delta) -> void:
 	lean_camera_towards_mouse_(delta)
+	last_physics_pos = position
 	match_player_position_()
