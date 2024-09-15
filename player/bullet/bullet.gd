da7190e4847eaaa2bb853adde937a7754c1be929
Logan Croley <logan.croley.18@gmail.com>
2024-09-15 17:22:37 -0400

    added variance between bullet shooting angle

--- player/bullet/bullet.gd
+++ player/bullet/bullet.gd
@@ -5,15 +5,20 @@ class_name Bullet
 
 var angle: Vector2
 var target: Vector2
+var variance: Vector2
 
 func _ready() -> void:
 	angle = global_position.direction_to(target)
+	print(angle)
+	angle += Vector2(randf_range(-0.15,0.15),0)
+	print(angle)
+
 	
 func _on_body_entered(body):
 	print("hit", body.name)
 	
 func _physics_process(delta: float) -> void:
-	position += angle* speed * delta
+	position += angle * speed * delta 
 
 func _on_queue_free_timer_timeout() -> void:
 	queue_free()
